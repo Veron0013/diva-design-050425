2c9f7f3f54d0b10336b0c085f11f6ce502767921
Mr.Igor <iv_mirsoft@ukr.net>
2025-04-05 18:08:57 +0300

    fix swiper

--- js/java_s.js
+++ js/java_s.js
@@ -1,42 +1,59 @@
+//console.log("java started");
+//var btnOpen = document.getElementById("open_form");
+//var btnClose = document.getElementById("close_form");
+//var myModalForm = document.getElementById("modal_form");
 
-var btnOpen = document.getElementById("open_form");
-var btnClose = document.getElementById("close_form");
-var myModalForm = document.getElementById("modal_form");
+//function toggleClass() {
+//	myModalForm.classList.toggle("is-open");
+//}
 
-function toggleClass() {
-	myModalForm.classList.toggle("is-open");
-}
-
-btnOpen.addEventListener("click", toggleClass);
-btnClose.addEventListener("click", toggleClass);
-
-import Swiper from 'swiper';
-import { Navigation, Pagination, Scrollbar } from 'swiper/modules';
+//btnOpen.addEventListener("click", toggleClass);
+//btnClose.addEventListener("click", toggleClass);
 
 // Now you can use Swiper
-const swiper = new Swiper('.swiper', {
-	// Optional parameters
-	direction: 'vertical',
-	autoplay: {
-		delay: 200,
-	},
-	spaceBetween: 24,
-	loop: true,
+document.addEventListener('DOMContentLoaded', function () {
+	console.log("java started");
+	const swiper = new Swiper('.swiper', {
 
-	// If we need pagination
-	pagination: {
-		el: '.swiper-pagination',
-	},
+		slidesPerView: 3,
+		spaceBetween: 24,
+		loop: true,
+		speed: 2000,
+		autoplay: {
+			delay: 0,
+			reverseDirection: false,
+			disableOnInteraction: false,
+			pauseOnMouseEnter: true,
+		},
+	});
 
-	// Navigation arrows
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
+	const swiper2 = new Swiper('.swiper-2', {
 
-	// And if we need scrollbar
-	scrollbar: {
-		el: '.swiper-scrollbar',
-	},
+		spaceBetween: 24,
+		loop: true,
+		speed: 2500,
+		slidesPerView: 3,
+		autoplay: {
+			delay: 0,
+			reverseDirection: true,
+			disableOnInteraction: false,
+			pauseOnMouseEnter: true,
+		},
+		loop: true,
+	});
+
+	const swiper3 = new Swiper('.swiper-3', {
+
+		slidesPerView: 3,
+		spaceBetween: 24,
+		loop: true,
+		speed: 3500,
+		autoplay: {
+			delay: 0,
+			reverseDirection: false,
+			disableOnInteraction: false,
+			pauseOnMouseEnter: true,
+		},
+	});
 });
 
