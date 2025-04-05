c2498cfc902da14564d0e4f1d330808ef43b83cb
Mr.Igor <iv_mirsoft@ukr.net>
2025-04-05 17:16:58 +0300

    swiper

--- js/java_s.js
+++ js/java_s.js
@@ -9,3 +9,34 @@ function toggleClass() {
 
 btnOpen.addEventListener("click", toggleClass);
 btnClose.addEventListener("click", toggleClass);
+
+import Swiper from 'swiper';
+import { Navigation, Pagination, Scrollbar } from 'swiper/modules';
+
+// Now you can use Swiper
+const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	direction: 'vertical',
+	autoplay: {
+		delay: 200,
+	},
+	spaceBetween: 24,
+	loop: true,
+
+	// If we need pagination
+	pagination: {
+		el: '.swiper-pagination',
+	},
+
+	// Navigation arrows
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
+
+	// And if we need scrollbar
+	scrollbar: {
+		el: '.swiper-scrollbar',
+	},
+});
+
